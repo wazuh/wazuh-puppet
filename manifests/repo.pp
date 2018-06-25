ec40715f666944e996b70b5a9697ecbe5a33e425
José Luis Ruiz <jose@wazuh.com>
2018-06-25 20:21:40 +0200

    Update Repo.pp for Amazon Linux servers.

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -35,7 +35,7 @@ class wazuh::repo (
     }
     'Linux', 'Redhat' : {
         case $::os[name] {
-          /^(CentOS|RedHat|OracleLinux|Fedora)$/: {
+          /^(CentOS|RedHat|OracleLinux|Fedora|Amazon)$/: {
             if ( $::operatingsystemrelease =~ /^5.*/ ) {
               $baseurl  = 'https://packages.wazuh.com/3.x/yum/5/'
               $gpgkey   = 'http://packages.wazuh.com/key/GPG-KEY-WAZUH-5'
