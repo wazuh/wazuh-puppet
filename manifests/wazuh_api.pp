f258bb4fdb45a029d74bedd6feab6e8138dd5dd1
Jose M <jm.garcia@wazuh.com>
2019-06-07 18:07:38 +0200

    Add "wazuh_api.pp"

---
+++ manifests/wazuh_api.pp
@@ -0,0 +1,30 @@
+
+class wazuh::wazuh_api (
+
+  $wazuh_api_package = "wazuh-api",
+  $wazuh_api_service = "wazuh-api",
+  $wazuh_api_version = "3.9.1-1",
+
+  $nodejs_package = "nodejs"
+
+){
+
+  exec { 'Installing node repository':
+    command => "cd /tmp && curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -",
+    provider => 'shell',
+  }
+  
+  package { $nodejs_package:
+  }
+
+  package { $wazuh_api_package:
+    ensure  => $wazuh_api_version,
+  }
+
+  service { "wazuh-api":
+    ensure  => running,
+    enable  => true,
+  }
+
+
+}
