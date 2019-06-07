ac31a02c5a6771e5e480db378934b23e2dc59b03
Jose M <jm.garcia@wazuh.com>
2019-06-07 18:07:26 +0200

    Created required files for Kibana.

---
+++ manifests/kibana.pp
@@ -0,0 +1,58 @@
+class wazuh::kibana (
+  $kibana_package = "kibana",
+  $kibana_service = "kibana",
+  $kibana_version = "7.1.1",
+  $kibana_app_version = "3.9.1_7.1.1",
+
+  $kibana_elasticsearch_ip = "172.17.0.101",
+  $kibana_elasticsearch_port = "9200",
+
+  $kibana_server_port = "5601",
+  $kibana_server_host = "0.0.0.0",
+  $kibana_elasticsearch_server_hosts ="http://${kibana_elasticsearch_ip}:${kibana_elasticsearch_port}",
+
+){
+
+  # install package
+  package { 'Installing Kibana...':
+    name    => $kibana_package,
+    ensure  => $kibana_version,
+  }
+
+  file { 'Configure kibana.yml':
+    owner   => 'kibana',
+    path    => '/etc/kibana/kibana.yml', 
+    group   => 'kibana',
+    mode    => '0644',
+    notify  => Service[$kibana_service], ## Restarts the service
+    content => template("wazuh/kibana_yml.erb"),
+  }
+
+  exec {"Installing Wazuh App...":
+    command => "sudo -u kibana /usr/share/kibana/bin/kibana-plugin install https://packages.wazuh.com/wazuhapp/wazuhapp-${kibana_app_version}.zip kibana",
+    creates => '/usr/share/kibana/plugins/wazuh/package.json',
+    provider => 'shell',
+  }
+
+  exec {"Waiting for elasticsearch...":
+    command => "until (curl -XGET http://${elasticsearch_ip}:${elasticsearch_port}); do\
+                printf 'Waiting for elasticsearch....'\
+                sleep 5\
+                done",
+    provider => 'shell',
+    returns => [0, 2, 14],
+  }
+
+  service { "kibana":
+    ensure  => running,
+    enable  => true,
+  }
+
+  exec { 'Verify Kibana folders owner':
+    command => "chown -R kibana:kibana /usr/share/kibana/optimize\
+             && chown -R kibana:kibana /usr/share/kibana/plugins",
+    provider => 'shell',             
+  }
+
+
+}
