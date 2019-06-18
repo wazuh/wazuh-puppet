b1693be16c0393b2d2bcfd7905dd774d3e54fbe5
Jose M <jm.garcia@wazuh.com>
2019-06-18 18:08:35 +0200

    Divide command  templates.

--- manifests/command.pp
+++ manifests/command.pp
@@ -4,14 +4,14 @@ define wazuh::command(
   $command_name,
   $command_executable,
   $command_expect = 'srcip',
-  $timeout_allowed = true,
+  $command_timeout_allowed = true,
 ) {
   require wazuh::params_manager
 
   if ($timeout_allowed) { $command_timeout_allowed='yes' } else { $command_timeout_allowed='no' }
   concat::fragment { $name:
     target  => 'ossec.conf',
-    order   => 45,
+    order   => 46,
     content => template('wazuh/command.erb'),
   }
 }
