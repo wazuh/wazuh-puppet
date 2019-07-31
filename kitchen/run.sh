279f910a8db3b06f9889ce54b612020ecb53e4de
Rshad Zhran <rashzk95@gmail.com>
2019-07-31 12:37:36 +0200

    added comments to kitchen/run.sh script

--- kitchen/run.sh
+++ kitchen/run.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
-rm -rf .kitchen/logs/*
-rm -rf .kitchen/def*
-rm -rf ./manifests/se*
-kitchen destroy all
-docker rm -f $(docker ps -aq)
-kitchen create
+rm -rf .kitchen/logs/* # removing old logs
+rm -rf .kitchen/def* # removing old .yml files associated for old kitchen instances
+rm -rf ./manifests/se* # removing all temporal manifests files.
+
+kitchen destroy all # destroying all existing kitchen instances
+docker rm -f $(docker ps -aq) # removing all existing containers.
+
+kitchen create # creating new kitchen instances
+
+# getting Wazuh managers IPs.
 ubuntu_manager_ip="$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' `docker ps | awk '{print $NF}' | grep  ubuntu | grep manager`)"
 centos_manager_ip="$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' `docker ps | awk '{print $NF}' | grep  centos | grep manager`)"
+
+# getting a backup of ./manifests/site.pp.template
 cp ./manifests/site.pp.template ./manifests/site.pp
+
+# Assigning Wazuh managers IPs to the corresponding agents.
 sed -i 's/ubuntu_manager_ip/'${ubuntu_manager_ip}'/g' ./manifests/site.pp
 sed -i 's/centos_manager_ip/'${centos_manager_ip}'/g' ./manifests/site.pp
+
+# Installing the configured wazuh manifests.
 kitchen converge
+
+# Testing
 kitchen verify
