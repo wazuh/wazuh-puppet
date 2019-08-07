59400f83028d1275ac165f99aaa8439bb81464d6
rshad <rashzk95@gmail.com>
2019-08-07 09:57:49 +0000

    made docker deletion more specific for kitchen containers

--- kitchen/clean.sh
+++ kitchen/clean.sh
@@ -8,8 +8,5 @@ rm -rf ./manifests/se* # removing all temporal manifests files.
 echo "Kitchen is destroying old instances ..."
 kitchen destroy all # destroying all existing kitchen instances
 
-echo "Docker is stopping old containers of they do exist"
-docker stop $(docker ps -a -q)
-
-echo "Docker is deleting the stopped containers"
-docker rm $(docker ps -a -q)
+echo "Docker is stopping and deleting old containers of they do exist"
+docker ps --filter name=kitchen -aq | xargs docker stop | xargs docker rm
