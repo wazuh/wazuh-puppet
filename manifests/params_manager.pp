504e5e39168d3c57a1a7398b74317926cfb8e4b3
Rshad Zhran <rashzk95@gmail.com>
2020-01-27 12:59:56 +0100

    Adapt logfiles list

--- manifests/params_manager.pp
+++ manifests/params_manager.pp
@@ -314,11 +314,8 @@ class wazuh::params_manager {
           $api_service_provider = undef
           $default_local_files = [
             {  'location' => '/var/log/syslog' , 'log_format' => 'syslog'},
-            {  'location' => '/var/log/kern.log' , 'log_format' => 'syslog'},
-            {  'location' => '/var/log/auth.log' , 'log_format' => 'syslog'},
             {  'location' => '/var/log/dpkg.log', 'log_format' => 'syslog'},
             {  'location' => '/var/ossec/logs/active-responses.log', 'log_format' => 'syslog'},
-            {  'location' => '/var/log/messages' , 'log_format' => 'syslog'},
           ]
           case $::lsbdistcodename {
             'xenial': {
