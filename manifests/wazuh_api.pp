5149941d3757bb1fd3574759eba7b5d11ec84d0f
Manuel Gutierrez <manuel.gutierrez@wazuh.com>
2020-03-11 13:22:18 +0100

    Moved nodejs class

--- manifests/wazuh_api.pp
+++ manifests/wazuh_api.pp
@@ -31,26 +31,3 @@ class wazuh::wazuh_api (
     require  => Package[$wazuh_api_package],
   }
 }
-
-class wazuh::wazuh_api::nodejs (
-  $nodejs_package = 'nodejs'
-){
-  if $::osfamily == 'Debian' {
-    exec { 'Updating repositories...':
-      path    => '/usr/bin',
-      command => 'curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -',
-    }
-    package { $nodejs_package:
-      provider => 'apt',
-    }
-  } else {
-    exec { 'Updating repositories...':
-      path    => '/usr/bin',
-      command => 'curl --silent --location https://rpm.nodesource.com/setup_10.x | bash -',
-
-    }
-    package { $nodejs_package:
-      provider => 'yum',
-    }
-  }
-}
