a87b0efdbf45ec38997448853c3456ff5d490d1c
Zenidd <jpsaezgutierrez@gmail.com>
2020-06-25 12:46:11 +0200

    Testing init

--- .github/actions/test_manager_and_agent/entrypoint.sh
+++ .github/actions/test_manager_and_agent/entrypoint.sh
@@ -17,5 +17,4 @@ cd .. && cp -r `ls -A | grep -v "kitchen"` kitchen/modules/wazuh/
 cd kitchen # Access kitchen folder
 
 echo "Kitchen is creating the new instances"
-kitchen init
-kitchen test
+kitchen list
