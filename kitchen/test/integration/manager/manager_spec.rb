147fda62a2a8487e76458cf77f643dab3c855f09
Zenidd <jpsaezgutierrez@gmail.com>
2020-06-25 18:14:16 +0200

    Manager spec minor fixes and bundle kitchen test launch

--- kitchen/test/integration/manager/manager_spec.rb
+++ kitchen/test/integration/manager/manager_spec.rb
@@ -1,9 +1,9 @@
 describe package('wazuh-manager') do
   it { should be_installed }
-  its('version') { should eq '3.12.3' }
+  its('version') { should eq '3.13.0-1' }
 end
 
-describe service('wazuh-managerj') do
+describe service('wazuh-manager') do
   it { should be_installed }
   it { should be_enabled }
   it { should be_running }
@@ -12,20 +12,20 @@ end
 # Verifying daemons
 
 wazuh_daemons = {
-                  "ossec-authd" => "root"),
-                  "ossec-execd" => "root"),
-                  "ossec-analysisd" => "ossec"),
-                  "ossec-syscheckd" => "root"),
-                  "ossec-remoted" => "ossecr"),
-                  "ossec-logcollector" => "root"),
-                  "ossec-monitord" => "ossec"),
-                  "wazuh-db" => "ossec"),
-                  "wazuh-modulesd" => "root"),
+                  "ossec-authd" => "root",
+                  "ossec-execd" => "root",
+                  "ossec-analysisd" => "ossec",
+                  "ossec-syscheckd" => "root",
+                  "ossec-remoted" => "ossecr",
+                  "ossec-logcollector" => "root",
+                  "ossec-monitord" => "ossec",
+                  "wazuh-db" => "ossec",
+                  "wazuh-modulesd" => "root"
                 }
 wazuh_daemons.each do |key, value|
 
   describe processes(key) do
-    its('USER') { should eq [value] }
+    its('users') { should eq [value] }
   end
 
 end
