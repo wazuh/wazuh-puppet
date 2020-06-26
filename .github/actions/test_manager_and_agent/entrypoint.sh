7c6468d05efdeaf3b42e98fd5ba9744dd8a0b20c
Zenidd <jpsaezgutierrez@gmail.com>
2020-06-26 12:55:40 +0200

    Access to kitchen directory

--- .github/actions/test_manager_and_agent/entrypoint.sh
+++ .github/actions/test_manager_and_agent/entrypoint.sh
@@ -6,11 +6,14 @@ echo `ls -lah`
 echo `git status`
 echo `git branch`
 
+
+
+cd kitchen # Access kitchen folder
+
 mkdir -p modules/wazuh
 
 cd .. && cp -r `ls -A | grep -v "kitchen"` kitchen/modules/wazuh/
 
-cd kitchen # Access kitchen folder
 
 echo "Installing dependencies"
 bundle install
@@ -27,14 +30,14 @@ cp ./manifests/site.pp.template ./manifests/site.pp
 echo "wazuh-manager IP"
 echo $manager_ip
 
-echo "Assigning Wazuh managers IPs to the corresponding agents."
-sed -i 's/manager_ip/'${manager_ip}'/g' ./manifests/site.pp
+# echo "Assigning Wazuh managers IPs to the corresponding agents."
+# sed -i 's/manager_ip/'${manager_ip}'/g' ./manifests/site.pp
 
-echo "Setting the platform in the components names."
-sed -i 's/platform/'$PLATFORM'/g' ./manifests/site.pp
+# echo "Setting the platform in the components names."
+# sed -i 's/platform/'$PLATFORM'/g' ./manifests/site.pp
 
-echo "Setting the rlease in the components names."
-sed -i 's/release/'$RELEASE'/g' ./manifests/site.pp
+# echo "Setting the rlease in the components names."
+# sed -i 's/release/'$RELEASE'/g' ./manifests/site.pp
 
 # if [[ $PLATFORM == *"centos"* ]] || [[ $PLATFORM == *"amazon"* ]]; then
 #    echo "suite is a Centos one and requires OpenSSL to be installed. .. Installing .."
