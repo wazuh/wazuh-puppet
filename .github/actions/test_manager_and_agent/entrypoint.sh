abc679f40a116e9a3aac55f47f9df3f2da0966cf
Zenidd <jpsaezgutierrez@gmail.com>
2020-06-26 12:36:43 +0200

    Adding more configuration steps on entrypoint

--- .github/actions/test_manager_and_agent/entrypoint.sh
+++ .github/actions/test_manager_and_agent/entrypoint.sh
@@ -22,23 +22,23 @@ bundle install
 echo "Kitchen is creating the new instances"
 bundle exec kitchen create
 
-# echo "Getting Wazuh managers IPs to the agents"
-# manager_ip="$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' `docker ps | awk '{print $NF}' | grep $PLATFORM | grep $RELEASE | grep manager`)"
+echo "Getting Wazuh managers IPs to the agents"
+manager_ip="$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' `docker ps | awk '{print $NF}' | grep $PLATFORM | grep $RELEASE | grep manager`)"
 
-# echo "getting a copy of ./manifests/site.pp.template"
-# cp ./manifests/site.pp.template ./manifests/site.pp
+echo "getting a copy of ./manifests/site.pp.template"
+cp ./manifests/site.pp.template ./manifests/site.pp
 
-# echo "wazuh-manager IP"
-# echo $manager_ip
+echo "wazuh-manager IP"
+echo $manager_ip
 
-# echo "Assigning Wazuh managers IPs to the corresponding agents."
-# sed -i 's/manager_ip/'${manager_ip}'/g' ./manifests/site.pp
+echo "Assigning Wazuh managers IPs to the corresponding agents."
+sed -i 's/manager_ip/'${manager_ip}'/g' ./manifests/site.pp
 
-# echo "Setting the platform in the components names."
-# sed -i 's/platform/'$PLATFORM'/g' ./manifests/site.pp
+echo "Setting the platform in the components names."
+sed -i 's/platform/'$PLATFORM'/g' ./manifests/site.pp
 
-# echo "Setting the rlease in the components names."
-# sed -i 's/release/'$RELEASE'/g' ./manifests/site.pp
+echo "Setting the rlease in the components names."
+sed -i 's/release/'$RELEASE'/g' ./manifests/site.pp
 
 # if [[ $PLATFORM == *"centos"* ]] || [[ $PLATFORM == *"amazon"* ]]; then
 #    echo "suite is a Centos one and requires OpenSSL to be installed. .. Installing .."
