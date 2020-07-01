93ca08bc52fb87791db4f98a73a193070b318062
Zenidd <jpsaezgutierrez@gmail.com>
2020-07-01 15:16:43 +0200

    amazon linux image

--- .github/actions/test_manager_and_agent/entrypoint.sh
+++ .github/actions/test_manager_and_agent/entrypoint.sh
@@ -26,8 +26,6 @@ bundle install
 echo "Kitchen is creating the new instances"
 bundle exec kitchen create
 
-
-
 echo "Getting Wazuh managers IPs to the agents"
 manager_ip="$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' `docker ps | awk '{print $NF}' | grep manager`)"
 
