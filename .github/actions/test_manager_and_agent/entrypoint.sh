5ff3b09ee832907e11406b317f828306f960e845
Zenidd <jpsaezgutierrez@gmail.com>
2020-07-01 12:42:01 +0200

    Centos7 testing

--- .github/actions/test_manager_and_agent/entrypoint.sh
+++ .github/actions/test_manager_and_agent/entrypoint.sh
@@ -26,8 +26,7 @@ bundle install
 echo "Kitchen is creating the new instances"
 bundle exec kitchen create
 
-echo "Current containers"
-echo `docker ps`
+
 
 echo "Getting Wazuh managers IPs to the agents"
 manager_ip="$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' `docker ps | awk '{print $NF}' | grep manager`)"
