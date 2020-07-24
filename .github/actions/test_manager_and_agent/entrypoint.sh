7c4055d4346b97dcc334190180660eb58f957b97
Zenidd <jpsaezgutierrez@gmail.com>
2020-07-24 13:32:03 +0200

    Debug

--- .github/actions/test_manager_and_agent/entrypoint.sh
+++ .github/actions/test_manager_and_agent/entrypoint.sh
@@ -1,55 +1,58 @@
 #! /usr/bin/env bash
 set -e
 
+echo "TEST WITH ECHO"
 echo `systemctl status`
-echo `ls -lah`
-echo `git status`
-echo `git branch`
-echo "Env vars value: IMAGE "
-echo $IMAGE
-echo "Env vars value: PLATFORM "
-echo $PLATFORM
-echo "Env vars value: RELEASE"
-echo $RELEASE
+echo "TEST WITH NO ECHO"
+systemctl status
+# echo `ls -lah`
+# echo `git status`
+# echo `git branch`
+# echo "Env vars value: IMAGE "
+# echo $IMAGE
+# echo "Env vars value: PLATFORM "
+# echo $PLATFORM
+# echo "Env vars value: RELEASE"
+# echo $RELEASE
 
-cd kitchen
+# cd kitchen
 
-mkdir -p modules/wazuh
+# mkdir -p modules/wazuh
 
-cd .. && cp -r `ls -A | grep -v "kitchen"` kitchen/modules/wazuh/
+# cd .. && cp -r `ls -A | grep -v "kitchen"` kitchen/modules/wazuh/
 
-cd kitchen
+# cd kitchen
 
-echo "Installing dependencies"
-bundle install
+# echo "Installing dependencies"
+# bundle install
 
-echo "Kitchen is creating the new instances"
-bundle exec kitchen create
+# echo "Kitchen is creating the new instances"
+# bundle exec kitchen create
 
-echo "Getting Wazuh managers IPs to the agents"
-manager_ip="$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' `docker ps | awk '{print $NF}' | grep manager`)"
+# echo "Getting Wazuh managers IPs to the agents"
+# manager_ip="$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' `docker ps | awk '{print $NF}' | grep manager`)"
 
-echo "getting a copy of ./manifests/site.pp.template"
-cp ./manifests/site.pp.template ./manifests/site.pp
+# echo "getting a copy of ./manifests/site.pp.template"
+# cp ./manifests/site.pp.template ./manifests/site.pp
 
-echo "wazuh-manager IP"
-echo $manager_ip
+# echo "wazuh-manager IP"
+# echo $manager_ip
 
-echo "Assigning Wazuh managers IPs to the corresponding agents."
-echo `sed -i 's/manager_ip/'${manager_ip}'/g' ./manifests/site.pp`
+# echo "Assigning Wazuh managers IPs to the corresponding agents."
+# echo `sed -i 's/manager_ip/'${manager_ip}'/g' ./manifests/site.pp`
 
-echo `cat ./manifests/site.pp`
+# echo `cat ./manifests/site.pp`
 
-if [[ $PLATFORM == *"centos"* ]] || [[ $PLATFORM == *"rhel"* ]]; then
-   echo "suite is a Centos one and requires OpenSSL to be installed. .. Installing .."
-   bundle exec kitchen exec $PLATFORM -c "sudo yum install -y openssl"
-fi
+# if [[ $PLATFORM == *"centos"* ]] || [[ $PLATFORM == *"rhel"* ]]; then
+#    echo "suite is a Centos one and requires OpenSSL to be installed. .. Installing .."
+#    bundle exec kitchen exec $PLATFORM -c "sudo yum install -y openssl"
+# fi
 
-echo "Kitchen is converging ..."
-bundle exec kitchen converge
+# echo "Kitchen is converging ..."
+# bundle exec kitchen converge
 
-echo "Kitchen is testing ..."
-bundle exec kitchen verify
+# echo "Kitchen is testing ..."
+# bundle exec kitchen verify
 
-echo "Kitchen is destroying"
-bundle exec kitchen destroy
+# echo "Kitchen is destroying"
+# bundle exec kitchen destroy
