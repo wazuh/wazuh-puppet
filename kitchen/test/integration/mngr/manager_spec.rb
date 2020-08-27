aee718beb45ad8ee382c4acf8b6e6792f7324335
zenidd <jpsaezgutierrez@gmail.com>
2020-08-27 15:19:23 +0200

    Adding ES checks to manager spec

--- kitchen/test/integration/mngr/manager_spec.rb
+++ kitchen/test/integration/mngr/manager_spec.rb
@@ -28,3 +28,36 @@ wazuh_daemons.each do |key, value|
     its('users') { is_expected.to eq [value] }
   end
 end
+
+describe package('elasticsearch') do
+  it { is_expected.to be_installed }
+  its('version') { is_expected.to eq '7.8.1-1' }
+end
+
+describe service('elasticsearch') do
+  it { is_expected.to be_installed }
+  it { is_expected.to be_enabled }
+  it { is_expected.to be_running }
+end
+
+describe package('filebeat') do
+  it { is_expected.to be_installed }
+  its('version') { is_expected.to eq '7.8.1-1' }
+end
+
+describe service('filebeat') do
+  it { is_expected.to be_installed }
+  it { is_expected.to be_enabled }
+  it { is_expected.to be_running }
+end
+
+describe package('kibana') do
+  it { is_expected.to be_installed }
+  its('version') { is_expected.to eq '7.8.1-1' }
+end
+
+describe service('kibana') do
+  it { is_expected.to be_installed }
+  it { is_expected.to be_enabled }
+  it { is_expected.to be_running }
+end
