131c224f865bd05fce8504c3b09c3e6b8a71fb15
zenidd <jpsaezgutierrez@gmail.com>
2020-08-31 14:23:39 +0200

    Set the correct test on OD arch

--- kitchen/test/integration/mngr_opendistro/manager_spec.rb
+++ kitchen/test/integration/mngr_opendistro/manager_spec.rb
@@ -79,6 +79,14 @@ elsif os.family == 'redhat'
   end
 end
 
+describe http('https://localhost:9200', auth: {user: 'admin', pass: 'admin'}, ssl_verify: false) do
+  its('status') { should cmp 200 }
+end
+
+describe http('https://localhost:9200', auth: {username: 'admin', password: 'admin'}, ssl_verify: false) do
+  its('status') { should cmp 200 }
+end
+
 describe http('https://localhost:9200', auth: {user: 'admin', password: 'admin'}, ssl_verify: false) do
   its('status') { should cmp 200 }
 end
