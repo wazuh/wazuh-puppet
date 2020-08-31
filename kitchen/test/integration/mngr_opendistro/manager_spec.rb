627a2684ffdb55f02200a05f16a33100074aa878
zenidd <jpsaezgutierrez@gmail.com>
2020-08-31 14:56:27 +0200

    Add kibana OD test

--- kitchen/test/integration/mngr_opendistro/manager_spec.rb
+++ kitchen/test/integration/mngr_opendistro/manager_spec.rb
@@ -83,10 +83,6 @@ describe http('https://localhost:9200', auth: {user: 'admin', pass: 'admin'}, ss
   its('status') { should cmp 200 }
 end
 
-# describe http('https://localhost:9200', auth: {username: 'admin', password: 'admin'}, ssl_verify: false) do
-#   its('status') { should cmp 200 }
-# end
-
-# describe http('https://localhost:9200', auth: {user: 'admin', password: 'admin'}, ssl_verify: false) do
-#   its('status') { should cmp 200 }
-# end
+describe http('http://localhost:5601/login') do
+  its('status') { should cmp 200 }
+end
