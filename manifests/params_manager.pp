47771a83fca781891d3fb023972483644d12cdf7
zenidd <jpsaezgutierrez@gmail.com>
2020-09-18 14:08:50 +0200

    Bump version to v3.13.2

--- manifests/params_manager.pp
+++ manifests/params_manager.pp
@@ -5,7 +5,7 @@ class wazuh::params_manager {
     'Linux': {
 
     # Installation
-      $server_package_version                          = '3.13.1-1'
+      $server_package_version                          = '3.13.2-1'
 
       $manage_repos                                    = true
       $manage_firewall                                 = false
@@ -491,7 +491,7 @@ class wazuh::params_manager {
       $keys_group = 'Administrators'
 
       $agent_service  = 'OssecSvc'
-      $agent_package  = 'Wazuh Agent 3.13.1'
+      $agent_package  = 'Wazuh Agent 3.13.2'
       $server_service = ''
       $server_package = ''
       $api_service = ''
