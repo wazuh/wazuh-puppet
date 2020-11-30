83ba91dbb3c842c3895403ebe4759354da219f3c
zenidd <jpsaezgutierrez@gmail.com>
2020-11-30 14:08:28 +0100

    agent_tests: nest describe statements under control block

--- kitchen/test/integration/agent/agent_spec.rb
+++ kitchen/test/integration/agent/agent_spec.rb
@@ -1,25 +1,32 @@
-describe package('wazuh-agent') do
-  it { is_expected.to be_installed }
-  its('version') { is_expected.to eq '4.0.2-1' }
-end
+control "agent" do
 
-describe service('wazuh-agent') do
-  it { is_expected.to be_installed }
-  it { is_expected.to be_enabled }
-  it { is_expected.to be_running }
-end
+  title "Wazuh agent tests"
+  describe "Checks Wazuh agent correct version, services and daemon ownership"
 
-# Verifying daemons
-
-wazuh_daemons = {
-  # 'ossec-agentd' => 'ossec',
-  'ossec-execd' => 'root',
-  # 'ossec-syscheckd' => 'root',
-#  'wazuh-modulesd' => 'root',
-}
-
-wazuh_daemons.each do |key, value|
-  describe processes(key) do
-    its('users') { is_expected.to eq [value] }
+  describe package('wazuh-agent') do
+    it { is_expected.to be_installed }
+    its('version') { is_expected.to eq '4.0.2-1' }
   end
+
+  describe service('wazuh-agent') do
+    it { is_expected.to be_installed }
+    it { is_expected.to be_enabled }
+    it { is_expected.to be_running }
+  end
+
+  # Verifying daemons
+
+  wazuh_daemons = {
+    # 'ossec-agentd' => 'ossec',
+    'ossec-execd' => 'root',
+    # 'ossec-syscheckd' => 'root',
+  #  'wazuh-modulesd' => 'root',
+  }
+
+  wazuh_daemons.each do |key, value|
+    describe processes(key) do
+      its('users') { is_expected.to eq [value] }
+    end
+  end
+
 end
