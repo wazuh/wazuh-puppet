3c04dc14a1952b0d438a0b79ede9a80f41f3e427
neonmei <neonmei@pm.me>
2020-12-02 14:53:32 -0300

    lib/facter: add file exists check to facter

--- lib/facter/kibana_plugin_wazuh.rb
+++ lib/facter/kibana_plugin_wazuh.rb
@@ -7,8 +7,11 @@ Facter.add('kibana_plugin_wazuh') do
   setcode do
     # move this to hiera
     wazuh_package_path = '/usr/share/kibana/plugins/wazuh/package.json'
-    f = File.read(wazuh_package_path)
-    kibana_plugin_wazuh = JSON.parse(f)
-    kibana_plugin_wazuh
+
+    if File.exist?(wazuh_package_path)
+      f = File.read(wazuh_package_path)
+      kibana_plugin_wazuh = JSON.parse(f)
+      kibana_plugin_wazuh
+    end
   end
 end
