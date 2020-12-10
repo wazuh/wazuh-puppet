2dec2ca8853e4f9d3e687735f65084ba09511de2
neonmei <neonmei@pm.me>
2020-12-10 10:27:37 -0300

    manifests: add Ubuntu Focal Fossa (20.04) to list of supported platforms in elastic repos

--- manifests/repo_elastic.pp
+++ manifests/repo_elastic.pp
@@ -15,7 +15,7 @@ class wazuh::repo_elastic (
           server => 'pgp.mit.edu'
         }
         case $::lsbdistcodename {
-          /(jessie|wheezy|stretch|buster|sid|precise|trusty|vivid|wily|xenial|yakketi|bionic)/: {
+          /(jessie|wheezy|stretch|buster|sid|precise|trusty|vivid|wily|xenial|yakketi|bionic|focal)/: {
 
             apt::source { 'wazuh_elastic':
               ensure   => present,
