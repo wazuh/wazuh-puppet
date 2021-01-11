c959134fcfa5827cd47e325e3a40f6224f042ffc
neonmei <neonmei@pm.me>
2021-01-11 11:36:34 -0300

    release: update wazuh-manager version to 4.0.4

--- manifests/params_manager.pp
+++ manifests/params_manager.pp
@@ -5,7 +5,7 @@ class wazuh::params_manager {
     'Linux': {
 
     # Installation
-      $server_package_version                          = '4.0.3-1'
+      $server_package_version                          = '4.0.4-1'
 
       $manage_repos                                    = true
       $manage_firewall                                 = false
@@ -534,7 +534,7 @@ class wazuh::params_manager {
       $keys_group = 'Administrators'
 
       $agent_service  = 'OssecSvc'
-      $agent_package  = 'Wazuh Agent 4.0.3'
+      $agent_package  = 'Wazuh Agent 4.0.4'
       $server_service = ''
       $server_package = ''
       $api_service = ''
