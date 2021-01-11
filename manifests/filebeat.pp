a2c9ef1a53952a81cd1410269884a1298b3d412b
neonmei <neonmei@pm.me>
2021-01-11 11:39:46 -0300

    release: update filebeat version to Wazuh 4.0.4

--- manifests/filebeat.pp
+++ manifests/filebeat.pp
@@ -8,8 +8,8 @@ class wazuh::filebeat (
   $filebeat_package = 'filebeat',
   $filebeat_service = 'filebeat',
   $filebeat_version = '7.9.3',
-  $wazuh_app_version = '4.0.3_7.9.3',
-  $wazuh_extensions_version = 'v4.0.3',
+  $wazuh_app_version = '4.0.4_7.9.3',
+  $wazuh_extensions_version = 'v4.0.4',
   $wazuh_filebeat_module = 'wazuh-filebeat-0.1.tar.gz',
 ){
 
