9882a10500829218ec80c3849094401a3ea5af5e
Kiriru Trofimov <kiriru.trofimov@gmail.com>
2021-01-13 13:06:43 +0100

    manifests/command.pp: Add $target_arg with default value and fix concat

--- manifests/command.pp
+++ manifests/command.pp
@@ -5,12 +5,13 @@ define wazuh::command(
   $command_executable,
   $command_expect = 'srcip',
   $timeout_allowed = true,
+  $target_arg      = 'manager_ossec.conf',
 ) {
   require wazuh::params_manager
 
   if ($timeout_allowed) { $command_timeout_allowed='yes' } else { $command_timeout_allowed='no' }
   concat::fragment { $name:
-    target  => 'ossec.conf',
+    target  => $target_arg,
     order   => 46,
     content => template('wazuh/fragments/_command.erb'),
   }
