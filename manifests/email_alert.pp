198bf8931974d1360c58f449d3db777dcd1d4638
Ante Jurjevic <ante.jurjevic@srce.hr>
2021-01-25 11:55:14 +0100

    Resolve conflicts in email_alert manifest

--- manifests/email_alert.pp
+++ manifests/email_alert.pp
@@ -1,19 +1,21 @@
-# Wazuh App Copyright (C) 2020 Wazuh Inc. (License GPLv2)
+
+# Wazuh App Copyright (C) 2021 Wazuh Inc. (License GPLv2)
 # Define an email alert
 define wazuh::email_alert(
   $alert_email,
-  $level = false,
-  $alert_group = false,
+  $alert_group    = false,
+  $target_arg     = 'manager_ossec.conf',
+  $level          = false,
   $event_location = false,
-  $format = false,
-  $rule_id = false,
-  $do_not_delay = false,
-  $do_not_group = false,
+  $format         = false,
+  $rule_id        = false,
+  $do_not_delay   = false,
+  $do_not_group   = false
 ) {
   require wazuh::params_manager
 
   concat::fragment { $name:
-    target  => 'ossec.conf',
+    target  => $target_arg,
     order   => 66,
     content => template('wazuh/fragments/_email_alert.erb'),
   }
