e987e5ac458ad781c9ee313875e90b7028ec7500
jcruzlp <jcruzlp97@gmail.com>
2021-05-14 10:39:08 +0200

    Updated agent integration test version

--- kitchen/test/integration/agent/agent_spec.rb
+++ kitchen/test/integration/agent/agent_spec.rb
@@ -1,6 +1,6 @@
 describe package('wazuh-agent') do
   it { is_expected.to be_installed }
-  its('version') { is_expected.to eq '3.13.2-1' }
+  its('version') { is_expected.to eq '3.13.3-1' }
 end
 
 describe service('wazuh-agent') do
