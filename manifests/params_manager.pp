9500bc854ace48d68e3c7f23bc43beb827a13378
vcerenu <victor.erenu@wazuh.com>
2021-11-12 10:42:53 -0300

    bump to 4.2.4

--- manifests/params_manager.pp
+++ manifests/params_manager.pp
@@ -5,7 +5,7 @@ class wazuh::params_manager {
     'Linux': {
 
     # Installation
-      $server_package_version                          = '4.3.0-1'
+      $server_package_version                          = '4.2.4-1'
 
       $manage_repos                                    = true
       $manage_firewall                                 = false
@@ -543,7 +543,7 @@ class wazuh::params_manager {
       $keys_group = 'Administrators'
 
       $agent_service  = 'WazuhSvc'
-      $agent_package  = 'Wazuh Agent 4.3.0'
+      $agent_package  = 'Wazuh Agent 4.2.4'
       $server_service = ''
       $server_package = ''
       $api_service = ''
