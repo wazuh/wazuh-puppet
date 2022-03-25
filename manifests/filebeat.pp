3730edb8e0bb203643ab6e0ed5eeac1a5ef3dc20
c-bordon <carlos.e.bordon@gmail.com>
2022-03-25 10:28:14 -0300

    4.2.6 Bump

--- manifests/filebeat.pp
+++ manifests/filebeat.pp
@@ -8,8 +8,8 @@ class wazuh::filebeat (
   $filebeat_package = 'filebeat',
   $filebeat_service = 'filebeat',
   $filebeat_version = '7.10.2',
-  $wazuh_app_version = '4.2.5_7.10.2',
-  $wazuh_extensions_version = 'v4.2.5',
+  $wazuh_app_version = '4.2.6_7.10.2',
+  $wazuh_extensions_version = 'v4.2.6',
   $wazuh_filebeat_module = 'wazuh-filebeat-0.1.tar.gz',
 ){
 
