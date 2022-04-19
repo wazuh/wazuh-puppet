a03f6663973be2e5023aa1a6f7fb8e30e2ea7dc3
Gonzalo Acuña <teddytpc1@gmail.com>
2022-04-19 18:31:52 -0300

    Audit package name updated for Centos

--- manifests/manager.pp
+++ manifests/manager.pp
@@ -344,7 +344,7 @@ class wazuh::manager (
         }
       }
       default: {
-        package { 'Installing Auditd...':
+        package { 'Installing Audit...':
           name => 'audit'
         }
       }
