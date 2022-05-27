d1626a99db91cb90f4a49b6984a623b8654aca2a
dfolcha <daniel.folch@wazuh.com>
2022-05-27 15:33:35 +0200

    Update filebeat module version

--- manifests/filebeat_oss.pp
+++ manifests/filebeat_oss.pp
@@ -12,7 +12,7 @@ class wazuh::filebeat_oss (
   $filebeat_oss_version = '7.10.2',
   $wazuh_app_version = '4.3.1_7.10.0',
   $wazuh_extensions_version = 'v4.3.1',
-  $wazuh_filebeat_module = 'wazuh-filebeat-0.1.tar.gz',
+  $wazuh_filebeat_module = 'wazuh-filebeat-0.2.tar.gz',
   $filebeat_path_certs = '/etc/filebeat/certs',
 ){
 
@@ -52,7 +52,7 @@ class wazuh::filebeat_oss (
   }
 
   exec { 'Unpackaging ...':
-    command => '/bin/tar -xzvf /root/wazuh-filebeat-0.1.tar.gz -C /usr/share/filebeat/module',
+    command => '/bin/tar -xzvf /root/wazuh-filebeat-0.2.tar.gz -C /usr/share/filebeat/module',
     notify  => Service[$filebeat_oss_service],
     require => Package[$filebeat_oss_package]
   }
