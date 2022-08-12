48e7474867807bd0781bb870935e7befd405e99d
vcerenu <victor.erenu@wazuh.com>
2022-08-12 12:04:42 -0300

    bump 4.3.7 version

--- manifests/indexer.pp
+++ manifests/indexer.pp
@@ -10,7 +10,7 @@ class wazuh::indexer (
   $indexer_node_max_local_storage_nodes = '1',
   $indexer_service = 'wazuh-indexer',
   $indexer_package = 'wazuh-indexer',
-  $indexer_version = '4.3.6-1',
+  $indexer_version = '4.3.7-1',
   $indexer_fileuser = 'wazuh-indexer',
   $indexer_filegroup = 'wazuh-indexer',
 
