efe506aba93d96dc80cae4e36f7b1e826da2e942
vcerenu <victor.erenu@wazuh.com>
2023-01-09 13:27:57 -0300

    add centos install

--- .github/scripts/centos_install.sh
+++ .github/scripts/centos_install.sh
@@ -5,16 +5,16 @@ ln -s /opt/puppetlabs/server/bin/puppetserver /bin
 echo "[main]" | sudo tee -a /etc/puppetlabs/puppet/puppet.conf > /dev/null
 echo "server = "$HOSTNAME | sudo tee -a /etc/puppetlabs/puppet/puppet.conf > /dev/null
 echo "dns_alt_names = "$HOSTNAME | sudo tee -a /etc/puppetlabs/puppet/puppet.conf > /dev/null
-sudo echo "127.0.0.1 puppet" | sudo tee -a /etc/hosts > /dev/null
-sudo cat /etc/puppetlabs/puppet/puppet.conf
-sudo systemctl start puppetserver
-sudo systemctl enable puppetserver
-sudo systemctl status puppetserver
+echo "127.0.0.1 puppet" | sudo tee -a /etc/hosts > /dev/null
+cat /etc/puppetlabs/puppet/puppet.conf
+systemctl start puppetserver
+systemctl enable puppetserver
+systemctl status puppetserver
 puppet module install /tmp/wazuh-wazuh-${{ env.VERSION }}.tar.gz
 hostname=$(sudo puppetserver ca list --all | awk '{if(NR>1)print $1;}')
-sudo echo "127.0.0.1 "$hostname | sudo tee -a /etc/hosts > /dev/null
-sudo echo "node "\"$hostname\"" {" | sudo tee -a /etc/puppetlabs/code/environments/production/manifests/stack.pp  > /dev/null
-sudo echo "class {'wazuh::manager':} ->  class {'wazuh::indexer':} -> class {'wazuh::filebeat_oss':} -> class {'wazuh::dashboard':}" | sudo tee -a /etc/puppetlabs/code/environments/production/manifests/stack.pp > /dev/null
-sudo echo "}" | sudo tee -a /etc/puppetlabs/code/environments/production/manifests/stack.pp > /dev/null
-sudo cat /etc/puppetlabs/code/environments/production/manifests/stack.pp
-sudo bash -c 'puppet agent -tod || test $? -eq 2'
+echo "127.0.0.1 "$hostname | sudo tee -a /etc/hosts > /dev/null
+echo "node "\"$hostname\"" {" | sudo tee -a /etc/puppetlabs/code/environments/production/manifests/stack.pp  > /dev/null
+echo "class {'wazuh::manager':} ->  class {'wazuh::indexer':} -> class {'wazuh::filebeat_oss':} -> class {'wazuh::dashboard':}" | sudo tee -a /etc/puppetlabs/code/environments/production/manifests/stack.pp > /dev/null
+echo "}" | sudo tee -a /etc/puppetlabs/code/environments/production/manifests/stack.pp > /dev/null
+cat /etc/puppetlabs/code/environments/production/manifests/stack.pp
+bash -c 'puppet agent -tod || test $? -eq 2'
