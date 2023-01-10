92c384ba3d5ff8624bb001782c703c2a9c5f090c
vcerenu <victor.erenu@wazuh.com>
2023-01-10 13:34:01 -0300

    test manifest for Wazuh manager install

--- .github/scripts/centos_install.sh
+++ .github/scripts/centos_install.sh
@@ -1,10 +1,10 @@
 rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
-ls _ltr /tmp
+ls -ltr /tmp
 yes | yum -y install puppet
 puppet module install /tmp/wazuh-wazuh-$VERSION.tar.gz
 mkdir /etc/puppet/manifests
 echo "node "\"$HOSTNAME\"" {" | tee -a /etc/puppet/manifests/wazuh.pp > /dev/null
-echo "class {'wazuh::manager'} ->  class {'wazuh::indexer':} -> class {'wazuh::filebeat_oss':} -> class {'wazuh::dashboard':}" | tee -a /etc/puppet/manifests/wazuh.pp > /dev/null
+echo "class {'wazuh::manager':} ->  class {'wazuh::indexer':} -> class {'wazuh::filebeat_oss':} -> class {'wazuh::dashboard':}" | tee -a /etc/puppet/manifests/wazuh.pp > /dev/null
 echo "}" | tee -a /etc/puppet/manifests/wazuh.pp > /dev/null
 cat /etc/puppet/manifests/wazuh.pp
-puppet apply /etc/puppet/manifests/wazuh.pp
+puppet apply -l /etc/puppet/manifests/wazuh.pp
