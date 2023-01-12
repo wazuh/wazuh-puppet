7de67d17a459be9b9ae822672714be49fd35474d
vcerenu <victor.erenu@wazuh.com>
2023-01-12 13:38:57 -0300

    change path for manifest

--- .github/scripts/centos_install.sh
+++ .github/scripts/centos_install.sh
@@ -3,7 +3,7 @@ ls -ltr /tmp
 yes | yum -y install puppet
 ln -s /opt/puppetlabs/bin/puppet /bin
 puppet module install /tmp/wazuh-wazuh-$VERSION.tar.gz
-echo "node "\"$HOSTNAME\"" {" | tee -a /etc/puppetlabs/code/environments/production/manifests/wazuh.pp > /dev/null
+echo "node \"locahost\" {" | tee -a /etc/puppetlabs/code/environments/production/manifests/wazuh.pp > /dev/null
 echo "  class {'wazuh::manager':} ->  class {'wazuh::indexer':} -> class {'wazuh::filebeat_oss':} -> class {'wazuh::dashboard':}" | tee -a /etc/puppetlabs/code/environments/production/manifests/wazuh.pp > /dev/null
 echo "}" | tee -a /etc/puppetlabs/code/environments/production/manifests/wazuh.pp > /dev/null
 cat /etc/puppetlabs/code/environments/production/manifests/wazuh.pp
