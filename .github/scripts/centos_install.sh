04fe96d355e33c5268a5a07244d1a6f1aee31319
vcerenu <victor.erenu@wazuh.com>
2023-01-12 11:54:23 -0300

    change Docker image for centos test

--- .github/scripts/centos_install.sh
+++ .github/scripts/centos_install.sh
@@ -1,12 +1,20 @@
-rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-7.noarch.rpm
-ls -ltr /tmp
-yes | yum -y install puppet
+rpm -Uvh https://yum.puppet.com/puppet7-release-el-8.noarch.rpm
+yum update -y
+yum install glibc -y
+yum -y install puppetserver
+ln -s /opt/puppetlabs/bin/puppet /bin
+ln -s /opt/puppetlabs/server/bin/puppetserver /bin
+echo "[main]" | tee -a /etc/puppetlabs/puppet/puppet.conf > /dev/null
+echo "server = "$HOSTNAME | tee -a /etc/puppetlabs/puppet/puppet.conf > /dev/null
+echo "dns_alt_names = "$HOSTNAME | tee -a /etc/puppetlabs/puppet/puppet.conf > /dev/null
+echo "127.0.0.1 puppet" | tee -a /etc/hosts > /dev/null
+cat /etc/puppetlabs/puppet/puppet.conf
+service puppetserver start
 puppet module install /tmp/wazuh-wazuh-$VERSION.tar.gz
-mkdir /etc/puppet/manifests
-echo "node "\"$HOSTNAME\"" {" | tee -a /etc/puppet/manifests/wazuh.pp > /dev/null
-echo "  class {'wazuh::manager':} ->  class {'wazuh::indexer':} -> class {'wazuh::filebeat_oss':} -> class {'wazuh::dashboard':}" | tee -a /etc/puppet/manifests/wazuh.pp > /dev/null
-echo "}" | tee -a /etc/puppet/manifests/wazuh.pp > /dev/null
-cat /etc/puppet/manifests/wazuh.pp
-puppet apply -l /etc/puppet/manifests/wazuh.pp
-puppet agent -tod
-ls -ltr /var/ossec
+hostname=$(puppetserver ca list --all | awk '{if(NR>1)print $1;}')
+echo "127.0.0.1 "$hostname | tee -a /etc/hosts > /dev/null
+echo "node "\"$HOSTNAME\"" {" | tee -a /etc/puppetlabs/code/environments/production/manifests/stack.pp  > /dev/null
+echo "class {'wazuh::manager':} ->  class {'wazuh::indexer':} -> class {'wazuh::filebeat_oss':} -> class {'wazuh::dashboard':}" | tee -a /etc/puppetlabs/code/environments/production/manifests/stack.pp > /dev/null
+echo "}" | tee -a /etc/puppetlabs/code/environments/production/manifests/stack.pp > /dev/null
+cat /etc/puppetlabs/code/environments/production/manifests/stack.pp
+bash -c 'puppet agent -tod || test $? -eq 2'
