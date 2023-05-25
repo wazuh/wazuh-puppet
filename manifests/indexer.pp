8269614282487927bc76efb58f1c8625aae45637
Raul Del Pozo Moreno <raul.delpozo@wazuh.com>
2023-05-25 22:01:08 +0200

    Bump to 4.4.3

--- manifests/indexer.pp
+++ manifests/indexer.pp
@@ -8,7 +8,7 @@ class wazuh::indexer (
   $indexer_node_max_local_storage_nodes = '1',
   $indexer_service = 'wazuh-indexer',
   $indexer_package = 'wazuh-indexer',
-  $indexer_version = '4.4.2-1',
+  $indexer_version = '4.4.3-1',
   $indexer_fileuser = 'wazuh-indexer',
   $indexer_filegroup = 'wazuh-indexer',
 
--- manifests/params_agent.pp
+++ manifests/params_agent.pp
@@ -1,7 +1,7 @@
 # Copyright (C) 2015, Wazuh Inc.
 # Wazuh-Agent configuration parameters
 class wazuh::params_agent {
-  $agent_package_version = '4.4.2'
+  $agent_package_version = '4.4.3'
   $agent_package_revision = '1'
   $agent_service_ensure = 'running'
   $agent_msi_download_location = 'https://packages.wazuh.com/4.x/windows'
