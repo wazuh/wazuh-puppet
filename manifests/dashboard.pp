d3999aee33e121c5c5ae0e391ca7ed7c6d96068f
Arnaud Launay <alaunay@cusae.com>
2023-06-27 00:13:12 +0200

    use correct version name

--- manifests/dashboard.pp
+++ manifests/dashboard.pp
@@ -3,7 +3,7 @@
 class wazuh::dashboard (
   $dashboard_package = 'wazuh-dashboard',
   $dashboard_service = 'wazuh-dashboard',
-  $dashboard_version = '4.4.4',
+  $dashboard_version = '4.4.4-1',
   $indexer_server_ip = 'localhost',
   $indexer_server_port = '9200',
   $dashboard_path_certs = '/etc/wazuh-dashboard/certs',
@@ -45,7 +45,7 @@ class wazuh::dashboard (
   # assign version according to the package manager
   case $facts['os']['family'] {
     'Debian': {
-      $dashboard_version_install = "${dashboard_version}-*"
+      $dashboard_version_install = "${dashboard_version}"
     }
     'Linux', 'RedHat', default: {
       $dashboard_version_install = $dashboard_version
