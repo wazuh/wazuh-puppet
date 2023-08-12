f97f225aeb5b36ba5fbea36a5133a95e419d1a92
Arnaud Launay <alaunay@cusae.com>
2023-08-12 10:55:33 +0200

    upgrade wazuh to 4.5.0

--- manifests/params_manager.pp
+++ manifests/params_manager.pp
@@ -5,7 +5,7 @@ class wazuh::params_manager {
     'Linux': {
 
     # Installation
-      $server_package_version                          = '4.4.5-1'
+      $server_package_version                          = '4.5.0-1'
 
       $manage_repos                                    = true
       $manage_firewall                                 = false
