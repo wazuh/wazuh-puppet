4403c8e4cc915768c6f8d6be0ad09a549659735c
c-bordon <carlos.e.bordon@gmail.com>
2023-11-13 12:02:07 -0300

    Testing ism init with hostname

--- manifests/securityadmin.pp
+++ manifests/securityadmin.pp
@@ -2,10 +2,11 @@
 # Wazuh repository installation
 class wazuh::securityadmin (
   $indexer_init_lockfile = '/var/tmp/indexer-init.lock',
+  $indexer_network_host = 'localhost',
 ) {
   exec { 'Initialize the Opensearch security index and ISM Polciy in Wazuh indexer':
     path    => ['/usr/bin', '/bin', '/usr/sbin', '/sbin'],
-    command => "/usr/share/wazuh-indexer/bin/indexer-init.sh && touch ${indexer_init_lockfile}",
+    command => "/usr/share/wazuh-indexer/bin/indexer-init.sh -i ${indexer_network_host} && touch ${indexer_init_lockfile}",
     creates => $indexer_init_lockfile,
     require => Service['wazuh-indexer'],
   }
