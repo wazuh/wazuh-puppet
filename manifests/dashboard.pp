d5cc4f0df8135d2d56d7a845171b363576185799
c-bordon <carlos.e.bordon@gmail.com>
2024-01-04 15:04:14 -0300

    update version

--- manifests/dashboard.pp
+++ manifests/dashboard.pp
@@ -3,7 +3,7 @@
 class wazuh::dashboard (
   $dashboard_package = 'wazuh-dashboard',
   $dashboard_service = 'wazuh-dashboard',
-  $dashboard_version = '4.8.2',
+  $dashboard_version = '4.9.0',
   $indexer_server_ip = 'localhost',
   $indexer_server_port = '9200',
   $manager_api_host = 'localhost',
