7f6d63d8260298de622b200b262e831bb0bb9e6f
David Correa Rodríguez <david.correa@wazuh.com>
2024-02-13 11:12:59 +0100

    Bump version to 5.0.0

--- manifests/params_manager.pp
+++ manifests/params_manager.pp
@@ -5,7 +5,7 @@ class wazuh::params_manager {
     'Linux': {
 
     # Installation
-      $server_package_version                          = '4.9.0-1'
+      $server_package_version                          = '5.0.0-1'
 
       $manage_firewall                                 = false
 
@@ -557,7 +557,7 @@ class wazuh::params_manager {
       $keys_group = 'Administrators'
 
       $agent_service  = 'WazuhSvc'
-      $agent_package  = 'Wazuh Agent 4.9.0'
+      $agent_package  = 'Wazuh Agent 5.0.0'
       $server_service = ''
       $server_package = ''
       $api_service = ''
