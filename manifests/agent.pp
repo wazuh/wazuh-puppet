327cd4d44b25b001d1082a1a168175682a2fcd4c
David Pearson <david.pearson1@digital.homeoffice.gov.uk>
2024-10-29 09:44:06 +0000

    Fix legacy facts & tidy formatting

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -91,7 +91,6 @@ class wazuh::agent (
   $wazuh_delay_after_enrollment      = $wazuh::params_agent::wazuh_delay_after_enrollment,
   $wazuh_enrollment_use_source_ip    = $wazuh::params_agent::wazuh_enrollment_use_source_ip,
 
-
   # Rootcheck
   $ossec_rootcheck_disabled           = $wazuh::params_agent::ossec_rootcheck_disabled,
   $ossec_rootcheck_check_files        = $wazuh::params_agent::ossec_rootcheck_check_files,
@@ -108,7 +107,6 @@ class wazuh::agent (
   $ossec_rootcheck_rootkit_trojans    = $wazuh::params_agent::ossec_rootcheck_rootkit_trojans,
   $ossec_rootcheck_skip_nfs           = $wazuh::params_agent::ossec_rootcheck_skip_nfs,
   $ossec_rootcheck_system_audit      = $wazuh::params_agent::ossec_rootcheck_system_audit,
-
 
   # rootcheck windows
   $ossec_rootcheck_windows_disabled        = $wazuh::params_agent::ossec_rootcheck_windows_disabled,
@@ -186,46 +184,46 @@ class wazuh::agent (
   $wodle_docker_listener_disabled    = $wazuh::params_agent::wodle_docker_listener_disabled,
 
   # Localfile
-  $ossec_local_files                 = $wazuh::params_agent::default_local_files,
+  $ossec_local_files = $wazuh::params_agent::default_local_files,
 
   # Syscheck
-  $ossec_syscheck_disabled           = $wazuh::params_agent::ossec_syscheck_disabled,
-  $ossec_syscheck_frequency          = $wazuh::params_agent::ossec_syscheck_frequency,
-  $ossec_syscheck_scan_on_start      = $wazuh::params_agent::ossec_syscheck_scan_on_start,
-  $ossec_syscheck_auto_ignore        = $wazuh::params_agent::ossec_syscheck_auto_ignore,
-  $ossec_syscheck_directories_1      = $wazuh::params_agent::ossec_syscheck_directories_1,
-  $ossec_syscheck_directories_2      = $wazuh::params_agent::ossec_syscheck_directories_2,
+  $ossec_syscheck_disabled      = $wazuh::params_agent::ossec_syscheck_disabled,
+  $ossec_syscheck_frequency     = $wazuh::params_agent::ossec_syscheck_frequency,
+  $ossec_syscheck_scan_on_start = $wazuh::params_agent::ossec_syscheck_scan_on_start,
+  $ossec_syscheck_auto_ignore   = $wazuh::params_agent::ossec_syscheck_auto_ignore,
+  $ossec_syscheck_directories_1 = $wazuh::params_agent::ossec_syscheck_directories_1,
+  $ossec_syscheck_directories_2 = $wazuh::params_agent::ossec_syscheck_directories_2,
 
-  $ossec_syscheck_report_changes_directories_1            = $wazuh::params_agent::ossec_syscheck_report_changes_directories_1,
-  $ossec_syscheck_whodata_directories_1            = $wazuh::params_agent::ossec_syscheck_whodata_directories_1,
-  $ossec_syscheck_realtime_directories_1           = $wazuh::params_agent::ossec_syscheck_realtime_directories_1,
-  $ossec_syscheck_report_changes_directories_2         = $wazuh::params_agent::ossec_syscheck_report_changes_directories_2,
-  $ossec_syscheck_whodata_directories_2            = $wazuh::params_agent::ossec_syscheck_whodata_directories_2,
-  $ossec_syscheck_realtime_directories_2           = $wazuh::params_agent::ossec_syscheck_realtime_directories_2,
-  $ossec_syscheck_ignore_list        = $wazuh::params_agent::ossec_syscheck_ignore_list,
-  $ossec_syscheck_ignore_type_1      = $wazuh::params_agent::ossec_syscheck_ignore_type_1,
-  $ossec_syscheck_ignore_type_2      = $wazuh::params_agent::ossec_syscheck_ignore_type_2,
+  $ossec_syscheck_report_changes_directories_1 = $wazuh::params_agent::ossec_syscheck_report_changes_directories_1,
+  $ossec_syscheck_whodata_directories_1        = $wazuh::params_agent::ossec_syscheck_whodata_directories_1,
+  $ossec_syscheck_realtime_directories_1       = $wazuh::params_agent::ossec_syscheck_realtime_directories_1,
+  $ossec_syscheck_report_changes_directories_2 = $wazuh::params_agent::ossec_syscheck_report_changes_directories_2,
+  $ossec_syscheck_whodata_directories_2        = $wazuh::params_agent::ossec_syscheck_whodata_directories_2,
+  $ossec_syscheck_realtime_directories_2       = $wazuh::params_agent::ossec_syscheck_realtime_directories_2,
+  $ossec_syscheck_ignore_list                  = $wazuh::params_agent::ossec_syscheck_ignore_list,
+  $ossec_syscheck_ignore_type_1                = $wazuh::params_agent::ossec_syscheck_ignore_type_1,
+  $ossec_syscheck_ignore_type_2                = $wazuh::params_agent::ossec_syscheck_ignore_type_2,
   $ossec_syscheck_max_eps                      = $wazuh::params_agent::ossec_syscheck_max_eps,
   $ossec_syscheck_process_priority             = $wazuh::params_agent::ossec_syscheck_process_priority,
   $ossec_syscheck_synchronization_enabled      = $wazuh::params_agent::ossec_syscheck_synchronization_enabled,
   $ossec_syscheck_synchronization_interval     = $wazuh::params_agent::ossec_syscheck_synchronization_interval,
   $ossec_syscheck_synchronization_max_eps      = $wazuh::params_agent::ossec_syscheck_synchronization_max_eps,
   $ossec_syscheck_synchronization_max_interval = $wazuh::params_agent::ossec_syscheck_synchronization_max_interval,
-  $ossec_syscheck_nodiff             = $wazuh::params_agent::ossec_syscheck_nodiff,
-  $ossec_syscheck_skip_nfs           = $wazuh::params_agent::ossec_syscheck_skip_nfs,
-  $ossec_syscheck_windows_audit_interval      = $wazuh::params_agent::windows_audit_interval,
+  $ossec_syscheck_nodiff                       = $wazuh::params_agent::ossec_syscheck_nodiff,
+  $ossec_syscheck_skip_nfs                     = $wazuh::params_agent::ossec_syscheck_skip_nfs,
+  $ossec_syscheck_windows_audit_interval       = $wazuh::params_agent::windows_audit_interval,
 
   # Audit
-  $audit_manage_rules                = $wazuh::params_agent::audit_manage_rules,
-  $audit_buffer_bytes                = $wazuh::params_agent::audit_buffer_bytes,
-  $audit_backlog_wait_time           = $wazuh::params_agent::audit_backlog_wait_time,
-  $audit_rules                       = $wazuh::params_agent::audit_rules,
+  $audit_manage_rules      = $wazuh::params_agent::audit_manage_rules,
+  $audit_buffer_bytes      = $wazuh::params_agent::audit_buffer_bytes,
+  $audit_backlog_wait_time = $wazuh::params_agent::audit_backlog_wait_time,
+  $audit_rules             = $wazuh::params_agent::audit_rules,
 
   # active-response
-  $ossec_active_response_disabled             =  $wazuh::params_agent::active_response_disabled,
-  $ossec_active_response_linux_ca_store       =  $wazuh::params_agent::active_response_linux_ca_store,
-  $ossec_active_response_ca_verification      =  $wazuh::params_agent::active_response_ca_verification,
-  $ossec_active_response_repeated_offenders   =  $wazuh::params_agent::active_response_repeated_offenders,
+  $ossec_active_response_disabled           =  $wazuh::params_agent::active_response_disabled,
+  $ossec_active_response_linux_ca_store     =  $wazuh::params_agent::active_response_linux_ca_store,
+  $ossec_active_response_ca_verification    =  $wazuh::params_agent::active_response_ca_verification,
+  $ossec_active_response_repeated_offenders =  $wazuh::params_agent::active_response_repeated_offenders,
 
   # Agent Labels
   $ossec_labels                      = $wazuh::params_agent::ossec_labels,
@@ -260,7 +258,6 @@ class wazuh::agent (
     }
   }
 
-
   if $manage_client_keys == 'yes' {
     if $wazuh_register_endpoint == undef {
       fail('The $wazuh_register_endpoint parameter is needed in order to register the Agent.')
@@ -268,7 +265,7 @@ class wazuh::agent (
   }
 
   # Package installation
-  case $::kernel {
+  case $facts['kernel'] {
     'Linux': {
       package { $agent_package_name:
         ensure => "${agent_package_version}-${agent_package_revision}", # lint:ignore:security_package_pinned_version
@@ -284,12 +281,12 @@ class wazuh::agent (
         group              => 'Administrators',
         mode               => '0774',
         source             => "${agent_msi_download_location}/wazuh-agent-${agent_package_version}-${agent_package_revision}.msi",
-        source_permissions => ignore
+        source_permissions => ignore,
       }
 
       # We dont need to pin the package version on Windows since we install if from the right MSI.
       -> package { $agent_package_name:
-        ensure          => "${agent_package_version}",
+        ensure          => "${ agent_package_version }",
         provider        => 'windows',
         source          => "${download_path}\\wazuh-agent-${agent_package_version}-${agent_package_revision}.msi",
         install_options => [
@@ -302,46 +299,44 @@ class wazuh::agent (
     default: { fail('OS not supported') }
   }
 
-  case $::kernel {
-  'Linux': {
-    ## ossec.conf generation concats
-    case $::operatingsystem {
-      'RedHat', 'OracleLinux', 'Suse':{
-        $apply_template_os = 'rhel'
-        if ( $::operatingsystemrelease =~ /^9.*/ ){
-          $rhel_version = '9'
-        }elsif ( $::operatingsystemrelease =~ /^8.*/ ){
-          $rhel_version = '8'
-        }elsif ( $::operatingsystemrelease =~ /^7.*/ ){
-          $rhel_version = '7'
-        }elsif ( $::operatingsystemrelease =~ /^6.*/ ){
-          $rhel_version = '6'
-        }elsif ( $::operatingsystemrelease =~ /^5.*/ ){
-          $rhel_version = '5'
-        }else{
-          fail('This ossec module has not been tested on your distribution')
+  case $facts['kernel'] {
+    'Linux': {
+      ## ossec.conf generation concats
+      case $facts['os']['name'] {
+        'RedHat', 'OracleLinux', 'Suse':{
+          $apply_template_os = 'rhel'
+          if ( $facts['os']['release']['full'] =~ /^9.*/ ) {
+            $rhel_version = '9'
+          } elsif ( $facts['os']['release']['full'] =~ /^8.*/ ) {
+            $rhel_version = '8'
+          } elsif ( $facts['os']['release']['full'] =~ /^7.*/ ) {
+            $rhel_version = '7'
+          } elsif ( $facts['os']['release']['full'] =~ /^6.*/ ) {
+            $rhel_version = '6'
+          } elsif ( $facts['os']['release']['full'] =~ /^5.*/ ) {
+            $rhel_version = '5'
+          } else {
+            fail('This ossec module has not been tested on your distribution')
+          }
+        } 'Debian', 'debian', 'Ubuntu', 'ubuntu':{
+          $apply_template_os = 'debian'
+          if ( $facts['os']['family']['codename']  == 'wheezy') or ( $facts['os']['family']['codename'] == 'jessie') {
+            $debian_additional_templates = 'yes'
+          }
+        } 'Amazon':{
+          $apply_template_os = 'amazon'
+        } 'CentOS','Centos','centos','AlmaLinux','Rocky':{
+          $apply_template_os = 'centos'
+        } 'SLES':{
+          $apply_template_os = 'suse'
         }
-      }'Debian', 'debian', 'Ubuntu', 'ubuntu':{
-        $apply_template_os = 'debian'
-        if ( $::lsbdistcodename == 'wheezy') or ($::lsbdistcodename == 'jessie'){
-          $debian_additional_templates = 'yes'
-        }
-      }'Amazon':{
-        $apply_template_os = 'amazon'
-      }'CentOS','Centos','centos','AlmaLinux','Rocky':{
-        $apply_template_os = 'centos'
-      }'SLES':{
-        $apply_template_os = 'suse'
+        default: { fail('OS not supported') }
       }
-      default: { fail('OS not supported') }
-    }
-  }'windows': {
+    } 'windows': {
       $apply_template_os = 'windows'
     }
     default: { fail('OS not supported') }
   }
-
-
   concat { 'agent_ossec.conf':
     path    => $wazuh::params_agent::config_file,
     owner   => $wazuh::params_agent::config_owner,
@@ -355,7 +350,7 @@ class wazuh::agent (
   concat::fragment {
     'ossec.conf_header':
       target  => 'agent_ossec.conf',
-      order   => 00,
+      order   => '00',
       before  => Service[$agent_service_name],
       content => "<ossec_config>\n";
     'ossec.conf_agent':
@@ -448,19 +443,19 @@ class wazuh::agent (
   }
   if ($configure_active_response == true) {
     wazuh::activeresponse { 'active-response configuration':
-      active_response_disabled           =>  $ossec_active_response_disabled,
-      active_response_linux_ca_store     =>  $ossec_active_response_linux_ca_store,
-      active_response_ca_verification    =>  $ossec_active_response_ca_verification,
-      active_response_repeated_offenders =>  $ossec_active_response_repeated_offenders,
+      active_response_disabled           => $ossec_active_response_disabled,
+      active_response_linux_ca_store     => $ossec_active_response_linux_ca_store,
+      active_response_ca_verification    => $ossec_active_response_ca_verification,
+      active_response_repeated_offenders => $ossec_active_response_repeated_offenders,
       order_arg                          => 40,
       before_arg                         => Service[$agent_service_name],
-      target_arg                         => 'agent_ossec.conf'
+      target_arg                         => 'agent_ossec.conf',
     }
   }
 
-  if ($configure_labels == true){
+  if ($configure_labels == true) {
     concat::fragment {
-        'ossec.conf_labels':
+      'ossec.conf_labels':
         target  => 'agent_ossec.conf',
         order   => 45,
         before  => Service[$agent_service_name],
@@ -504,9 +499,9 @@ class wazuh::agent (
       $agent_auth_option_address = ''
     }
 
-    case $::kernel {
+    case $facts['kernel'] {
       'Linux': {
-        file { $::wazuh::params_agent::keys_file:
+        file { $wazuh::params_agent::keys_file:
           owner => $wazuh::params_agent::keys_owner,
           group => $wazuh::params_agent::keys_group,
           mode  => $wazuh::params_agent::keys_mode,
@@ -567,7 +562,7 @@ class wazuh::agent (
         exec { 'agent-auth-linux':
           path    => ['/usr/bin', '/bin', '/usr/sbin', '/sbin'],
           command => $agent_auth_command,
-          unless  => "egrep -q '.' ${::wazuh::params_agent::keys_file}",
+          unless  => "egrep -q '.' ${wazuh::params_agent::keys_file}",
           require => Concat['agent_ossec.conf'],
           before  => Service[$agent_service_name],
           notify  => Service[$agent_service_name],
@@ -595,7 +590,7 @@ class wazuh::agent (
         exec { 'agent-auth-windows':
           command  => $agent_auth_command,
           provider => 'powershell',
-          onlyif   => "if ((Get-Item '${$::wazuh::params_agent::keys_file}').length -gt 0kb) {exit 1}",
+          onlyif   => "if ((Get-Item '${$wazuh::params_agent::keys_file}').length -gt 0kb) {exit 1}",
           require  => Concat['agent_ossec.conf'],
           before   => Service[$agent_service_name],
           notify   => Service[$agent_service_name],
@@ -625,7 +620,7 @@ class wazuh::agent (
 
   # SELinux
   # Requires selinux module specified in metadata.json
-  if ($::osfamily == 'RedHat' and $selinux == true) {
+  if ( $facts['os']['family'] == 'RedHat' and $selinux == true ) {
     selinux::module { 'ossec-logrotate':
       ensure    => 'present',
       source_te => 'puppet:///modules/wazuh/ossec-logrotate.te',
@@ -656,5 +651,4 @@ class wazuh::agent (
       require => Package[$wazuh::params_agent::agent_package_name],
     }
   }
-
 }
--- manifests/repo.pp
+++ manifests/repo.pp
@@ -2,31 +2,30 @@
 # Wazuh repository installation
 class wazuh::repo (
 ) {
-
-  case $::osfamily {
+  case $facts['os']['family'] {
     'Debian' : {
       $wazuh_repo_url = 'https://packages.wazuh.com/4.x/apt'
       $repo_release = 'stable'
 
-      if $::lsbdistcodename =~ /(jessie|wheezy|stretch|precise|trusty|vivid|wily|xenial|yakketi|groovy)/
+      if $facts['os']['family']['codename'] =~ /(jessie|wheezy|stretch|precise|trusty|vivid|wily|xenial|yakketi|groovy)/
       and ! defined(Package['apt-transport-https']) and ! defined(Package['gnupg']) {
-        ensure_packages(['apt-transport-https', 'gnupg'], {'ensure' => 'present'})
+        ensure_packages(['apt-transport-https', 'gnupg'], { 'ensure' => 'present' })
       }
       exec { 'import-wazuh-key':
-        path =>  [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+        path    => ['/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/'],
         command => 'curl -s https://packages.wazuh.com/key/GPG-KEY-WAZUH | gpg --no-default-keyring --keyring /usr/share/keyrings/wazuh.gpg --import',
         unless  => 'gpg --no-default-keyring --keyring /usr/share/keyrings/wazuh.gpg --list-keys | grep -q 29111145',
       }
 
       # Ensure permissions on the keyring
       file { '/usr/share/keyrings/wazuh.gpg':
-        ensure => file,
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0644',
+        ensure  => file,
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
         require => Exec['import-wazuh-key'],
       }
-      case $::lsbdistcodename {
+      case $facts['os']['family']['codename'] {
         /(jessie|wheezy|stretch|buster|bullseye|bookworm|sid|precise|trusty|vivid|wily|xenial|yakketi|bionic|focal|groovy|jammy)/: {
           apt::source { 'wazuh':
             ensure   => present,
@@ -38,19 +37,19 @@ class wazuh::repo (
               'src' => false,
               'deb' => true,
             },
-            require => File['/usr/share/keyrings/wazuh.gpg'],
+            require  => File['/usr/share/keyrings/wazuh.gpg'],
           }
           # Manage the APT source list file content using concat
           concat { '/etc/apt/sources.list.d/wazuh.list':
-            ensure  => present,
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0644',
+            ensure => present,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0644',
           }
 
           concat::fragment { 'wazuh-source':
             target  => '/etc/apt/sources.list.d/wazuh.list',
-            content => "deb [signed-by=/usr/share/keyrings/wazuh.gpg] $wazuh_repo_url $repo_release main\n",
+            content => "deb [signed-by=/usr/share/keyrings/wazuh.gpg] ${wazuh_repo_url} ${repo_release} main\n",
             order   => '01',
             require => File['/usr/share/keyrings/wazuh.gpg'],
           }
@@ -65,43 +64,44 @@ class wazuh::repo (
       }
     }
     'Linux', 'RedHat', 'Suse' : {
-        case $::os[name] {
-          /^(CentOS|RedHat|OracleLinux|Fedora|Amazon|AlmaLinux|Rocky|SLES)$/: {
-
-            if ( $::operatingsystemrelease =~ /^5.*/ ) {
-              $baseurl  = 'https://packages.wazuh.com/4.x/yum/5/'
-              $gpgkey   = 'http://packages.wazuh.com/key/GPG-KEY-WAZUH'
-            } else {
-              $baseurl  = 'https://packages.wazuh.com/4.x/yum/'
-              $gpgkey   = 'https://packages.wazuh.com/key/GPG-KEY-WAZUH'
-            }
-          }
-          default: { fail('This ossec module has not been tested on your distribution.') }
-        }
-        # Set up OSSEC repo
-        case $::os[name] {
-          /^(CentOS|RedHat|OracleLinux|Fedora|Amazon|AlmaLinux)$/: {
-            yumrepo { 'wazuh':
-              descr    => 'WAZUH OSSEC Repository - www.wazuh.com',
-              enabled  => true,
-              gpgcheck => 1,
-              gpgkey   => $gpgkey,
-              baseurl  => $baseurl
-            }
-          }
-          /^(SLES)$/: {
-            zypprepo { 'wazuh':
-              ensure        => present,
-              name          => 'WAZUH OSSEC Repository - www.wazuh.com',
-              enabled       => 1,
-              gpgcheck      => 0,
-              repo_gpgcheck => 0,
-              pkg_gpgcheck  => 0,
-              gpgkey        => $gpgkey,
-              baseurl       => $baseurl
-            }
+      case $facts['os']['name'] {
+        /^(CentOS|RedHat|OracleLinux|Fedora|Amazon|AlmaLinux|Rocky|SLES)$/: {
+          if ( $facts['os']['release']['full'] =~ /^5.*/ ) {
+            $baseurl  = 'https://packages.wazuh.com/4.x/yum/5/'
+            $gpgkey   = 'http://packages.wazuh.com/key/GPG-KEY-WAZUH'
+          } else {
+            $baseurl  = 'https://packages.wazuh.com/4.x/yum/'
+            $gpgkey   = 'https://packages.wazuh.com/key/GPG-KEY-WAZUH'
           }
         }
+        default: { fail('This ossec module has not been tested on your distribution.') }
+      }
+      # Set up OSSEC repo
+      case $facts['os']['name'] {
+        /^(CentOS|RedHat|OracleLinux|Fedora|Amazon|AlmaLinux)$/: {
+          yumrepo { 'wazuh':
+            descr    => 'WAZUH OSSEC Repository - www.wazuh.com',
+            enabled  => true,
+            gpgcheck => 1,
+            gpgkey   => $gpgkey,
+            baseurl  => $baseurl,
+          }
+        }
+        /^(SLES)$/: {
+          zypprepo { 'wazuh':
+            ensure        => present,
+            name          => 'WAZUH OSSEC Repository - www.wazuh.com',
+            enabled       => 1,
+            gpgcheck      => 0,
+            repo_gpgcheck => 0,
+            pkg_gpgcheck  => 0,
+            gpgkey        => $gpgkey,
+            baseurl       => $baseurl,
+          }
+        }
+        default: { fail('This ossec module has not been tested on your distribution.') }
+      }
     }
+    default: { fail('This ossec module has not been tested on your distribution.') }
   }
 }
