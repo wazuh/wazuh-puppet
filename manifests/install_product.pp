2df68e0b38e58f3d004704bd29da62d78b6ff98c
vcerenu <victor.erenu@wazuh.com>
2024-12-27 17:18:28 -0300

    Add new class

--- manifests/install_product.pp
+++ manifests/install_product.pp
@@ -98,12 +98,6 @@ class wazuh::install_product (
     warning("URL for ${package_pattern} not found in ${destination}")
   }
 
-  # Remove the downloaded URL list file.
-  file { $destination:
-    ensure => absent,
-    force  => true,
-  }
-
   file { "${download_dir}/package_url":
     ensure => absent,
     force  => true,
