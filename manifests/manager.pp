db4c789f0c87f72cc03f6b0fbd9efcdd81f3921e
vcerenu <victor.erenu@wazuh.com>
2024-12-27 13:05:35 -0300

    Add paramete

--- manifests/manager.pp
+++ manifests/manager.pp
@@ -1,25 +1,27 @@
 # Copyright (C) 2015, Wazuh Inc.
 # Main ossec server config
 class wazuh::manager (
+  String $version = '4.9.2',
+) {
 
-  $version = '4.9.2',
-){
-
+  # Instalar Wazuh Manager
   wazuh::install_product { 'wazuh-manager':
-    package_name    => 'wazuh-manager',
-    wazuh_version => $version,
+    package_name   => 'wazuh-manager',
+    wazuh_version  => $version,
   }
 
-  # Configure specific files for Wazuh Manager
-  wazuh::modify_config_file {
-    config_file => '/car/ossec/etc/ossec.conf',
-    config_lines => [],
-    file_type => 'xml'
+  # Configurar archivos específicos para Wazuh Manager
+  wazuh::modify_config_file { 'ossec_conf':
+    config_file    => '/var/ossec/etc/ossec.conf',
+    config_lines   => ['<server>enabled</server>'], # Añadir configuraciones específicas
+    file_type      => 'xml',
+    replace_all    => false,
   }
 
-
+  # Administrar el servicio
   service { 'wazuh-manager':
-    ensure => running,
-    enable => true,
+    ensure    => running,
+    enable    => true,
+    require   => Wazuh::Install_product['wazuh-manager'], # Asegurar que se instala antes de gestionar el servicio
   }
 }
