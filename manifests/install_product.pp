24a5493e4c4847e0ed0cad53503e397559ca5d84
vcerenu <victor.erenu@wazuh.com>
2024-12-27 13:12:40 -0300

    Add paramete

--- manifests/install_product.pp
+++ manifests/install_product.pp
@@ -2,9 +2,9 @@ class wazuh::install_product (
   String $package_name,
   String $wazuh_version = '5.0.0',
   String $prod_url       = 'https://devops-wazuh-artifacts-pub.s3.us-west-1.amazonaws.com/devops-overhaul/packages_url.txt',
-  Optional[String] $expected_checksum = undef, # Optional checksum for package verification
-  String $download_dir    = '/tmp', # Configurable download directory
-  Optional[String] $custom_url_file = undef, # Optional path to a local URL file
+  Optional[String] $expected_checksum = undef,
+  String $download_dir    = '/tmp',
+  Optional[String] $custom_url_file = undef,
 ) {
   String $destination = "${download_dir}/packages_url.txt"
   String $rpm_based = 'RedHat|Suse|Amazon|OracleLinux|AlmaLinux|Rocky'
@@ -29,75 +29,68 @@ class wazuh::install_product (
 
   # Determine the source of the URL file (local or remote)
   $source_url = $custom_url_file ? {
-    undef   => $prod_url, # Use the remote URL if no local file is provided
-    default => $custom_url_file, # Use the local file if provided
+    undef   => $prod_url,
+    default => $custom_url_file,
   }
 
-  # Download/copy the URL file based on the source
+  # Download/copy the URL file
   if $source_url == $prod_url {
-    # Download from remote URL
     exec { 'fetch_packages_url':
-      command     => "/usr/bin/curl --fail --location -o ${destination} ${prod_url}",
-      path         => ['/usr/bin', '/bin'],
-      creates     => $destination,
+      command  => "/usr/bin/curl --fail --location -o ${destination} ${prod_url}",
+      path     => ['/usr/bin', '/bin'],
+      creates  => $destination,
       logoutput => true,
     }
   } else {
-    # Copy from local file
     file { $destination:
       ensure => file,
       source => $source_url,
-      mode  => '0644',
+      mode   => '0644',
     }
   }
 
-  # Find the package URL (only if it's not installed)
-  $package_url = undef
+  # Find the package URL
   exec { "find_${package_pattern}_in_file":
-    command     => "/bin/grep -E '^${package_pattern}:' ${destination} | cut -d':' -f2",
-    path         => ['/bin', '/usr/bin'],
-    returns     => [0, 1], # Allow grep to not find anything (return code 1)
-    require     => $source_url == $prod_url ? { true => Exec['fetch_packages_url'], default => undef }, #Conditional requirement
+    command  => "/bin/grep -E '^${package_pattern}:' ${destination} | cut -d':' -f2 > ${download_dir}/package_url",
+    path     => ['/bin', '/usr/bin'],
+    creates  => "${download_dir}/package_url",
+    require  => File[$destination],
     logoutput => true,
-    unless => $check_command,
   }
 
-  # Assign the URL to a variable if grep was successful
-    if $?("find_${package_pattern}_in_file") == 0 {
-        $package_url = $execoutput("find_${package_pattern}_in_file")
-    }
+  # Read the URL from the temporary file
+  $package_url = file("${download_dir}/package_url")
 
-  if $package_url {
+  if $package_url != '' {
     $package_file = "${download_dir}/${package_pattern}"
 
-    # Download the package using the archive module with checksum verification
+    # Download the package
     archive { $package_file:
-      source       => $package_url,
-      checksum     => $expected_checksum ? { undef => undef, default => 'sha256' }, # Conditionally set checksum type
-      checksum_value => $expected_checksum,
-      creates      => $package_file,
-      require     => Exec["find_${package_pattern}_in_file"],
+      source          => $package_url,
+      checksum        => $expected_checksum ? { undef => undef, default => 'sha256' },
+      checksum_value  => $expected_checksum,
+      creates         => $package_file,
+      require         => Exec["find_${package_pattern}_in_file"],
     }
 
-    # Install the package with command injection protection
-    if $package_type == 'rpm' {
-      $install_command = ['/bin/rpm', '-ivh', $package_file]
-    } else {
-      $install_command = ['/usr/bin/dpkg', '-i', $package_file]
+    # Install the package
+    $install_command = $package_type ? {
+      'rpm' => "/bin/rpm -ivh ${package_file}",
+      'deb' => "/usr/bin/dpkg -i ${package_file}",
     }
 
     exec { "install_${package_pattern}":
-      command     => $install_command,
-      path         => ['/bin', '/usr/bin'],
-      require     => Archive[$package_file],
-      unless     => $check_command,
-      logoutput  => true,
+      command  => $install_command,
+      path     => ['/bin', '/usr/bin'],
+      require  => Archive[$package_file],
+      unless   => $check_command,
+      logoutput => true,
     }
 
     # Clean up the downloaded package file
     file { $package_file:
       ensure => absent,
-      force => true,
+      force  => true,
     }
   } else {
     warning("URL for ${package_pattern} not found in ${destination}")
@@ -106,6 +99,12 @@ class wazuh::install_product (
   # Clean up the URL file
   file { $destination:
     ensure => absent,
-    force => true,
+    force  => true,
+  }
+
+  # Clean up the temporary package URL file
+  file { "${download_dir}/package_url":
+    ensure => absent,
+    force  => true,
   }
 }
