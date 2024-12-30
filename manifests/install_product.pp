3abc8981c4b0163a48a690bf808d58c4c1adfcdd
vcerenu <victor.erenu@wazuh.com>
2024-12-30 10:07:13 -0300

    test install product

--- manifests/install_product.pp
+++ manifests/install_product.pp
@@ -97,4 +97,10 @@ class wazuh::install_product (
   } else {
     warning("URL for ${package_pattern} not found in ${destination}")
   }
+
+  # Remove the downloaded URL list file.
+  file { "${download_dir}/package_url":
+    ensure => absent,
+    force  => true,
+  }
 }
