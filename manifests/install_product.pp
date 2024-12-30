33768b8fec455af85ee05e9ffde7c9754822fe26
vcerenu <victor.erenu@wazuh.com>
2024-12-30 14:22:33 -0300

    test install product

--- manifests/install_product.pp
+++ manifests/install_product.pp
@@ -84,7 +84,7 @@ class wazuh::install_product (
     $package_file = "${download_dir}/${package_pattern}"
 
     exec { 'download_packages':
-      command   => "/usr/bin/curl -o ${package_file} ${package_url}",
+      command   => "curl -o ${package_file} ${package_url}",
       path      => ['/usr/bin', '/bin'],
       logoutput => true,
     }
