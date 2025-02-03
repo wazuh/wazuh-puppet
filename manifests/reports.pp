6c9f384104c2fb8c80da552489b5a9a9359afe1e
David Pearson <david.pearson1@digital.homeoffice.gov.uk>
2025-02-03 08:18:28 +0000

    Puppet 8 changes
    drop deprecated function validate_legacy
    
    use variables for cert content
    
    general format tidying
    
    fix indexer_cluster_cn

--- manifests/reports.pp
+++ manifests/reports.pp
@@ -1,6 +1,6 @@
 # Copyright (C) 2015, Wazuh Inc.
 #Define for a Reports section
-define wazuh::reports(
+define wazuh::reports (
   Optional[String] $r_group               = undef,
   Optional[String] $r_category            = undef,
   Optional[Integer] $r_rule               = undef,
@@ -12,12 +12,11 @@ define wazuh::reports(
   $r_email_to                             = '',
   Optional[Enum['yes', 'no']] $r_showlogs = undef,
 ) {
-
   require wazuh::params_manager
 
   concat::fragment { $name:
     target  => 'manager_ossec.conf',
     order   => 70,
-    content => template('wazuh/fragments/_reports.erb')
+    content => template('wazuh/fragments/_reports.erb'),
   }
 }
