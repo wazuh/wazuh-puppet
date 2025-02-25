0be6cb425578514d64321937334a595c3368d498
vcerenu <victor.erenu@wazuh.com>
2025-02-25 13:20:53 -0300

    New module

--- manifests/install_package.pp
+++ manifests/install_package.pp
@@ -34,7 +34,7 @@ define wazuh::install_package (
     command => "sh -c 'url=\$(grep -F '${package}:' /tmp/packages_url.txt | tr -d \"\\r\" | cut -d \" \" -f2); curl -o /tmp/${package} \"\$url\"'",
     unless  => "test -f /tmp/${package} && dpkg -I /tmp/${package} >/dev/null 2>&1",
     path    => ['/usr/bin', '/bin', '/sbin'],
-    timeout => 600,
+    timeout => 1200,
   }
 
   # Install the package using correct provider
