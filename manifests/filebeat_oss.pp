add5b05788765408f4fe9e212a22045a12820879
Gonzalo Acuña <teddytpc1@gmail.com>
2025-04-09 12:07:02 -0300

    Bump to 4.12.1

--- manifests/filebeat_oss.pp
+++ manifests/filebeat_oss.pp
@@ -11,8 +11,8 @@ class wazuh::filebeat_oss (
   $filebeat_oss_elastic_user = 'admin',
   $filebeat_oss_elastic_password = 'admin',
   $filebeat_oss_version = '7.10.2',
-  $wazuh_app_version = '4.12.0_7.10.2',
-  $wazuh_extensions_version = 'v4.12.0',
+  $wazuh_app_version = '4.12.1_7.10.2',
+  $wazuh_extensions_version = 'v4.12.1',
   $wazuh_filebeat_module = 'wazuh-filebeat-0.4.tar.gz',
   $wazuh_node_name = 'master',
 
