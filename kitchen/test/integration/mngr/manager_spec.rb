37127d2cf6945a0127199f78f3ecc08a16322995
vcerenu <victor.erenu@wazuh.com>
2025-05-06 14:04:23 -0300

    Bump 4.12.2 version

--- kitchen/test/integration/mngr/manager_spec.rb
+++ kitchen/test/integration/mngr/manager_spec.rb
@@ -4,7 +4,7 @@ control 'wazuh-manager' do
 
   describe package('wazuh-manager') do
     it { is_expected.to be_installed }
-    its('version') { is_expected.to eq '4.12.1-1' }
+    its('version') { is_expected.to eq '4.12.2-1' }
   end
 
   # Verifying service
