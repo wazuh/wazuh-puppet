ed8b92bf00d4a0ffd0a3ac190e65254003b1bf81
c-bordon <carlos.e.bordon@gmail.com>
2025-05-13 14:54:21 -0300

    Bumped 6.0.0 branch to 6.0.0 version

--- kitchen/test/integration/mngr/manager_spec.rb
+++ kitchen/test/integration/mngr/manager_spec.rb
@@ -4,7 +4,7 @@ control 'wazuh-manager' do
 
   describe package('wazuh-manager') do
     it { is_expected.to be_installed }
-    its('version') { is_expected.to eq '5.0.0-1' }
+    its('version') { is_expected.to eq '6.0.0-1' }
   end
 
   # Verifying service
