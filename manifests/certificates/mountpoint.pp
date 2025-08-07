8d66d744ccb2b3a674dd71d6b08696ff0d5de94d
griggi-ws <gavin.riggi@wellspring.com>
2025-08-07 17:24:02 -0400

    fix: use ` ` as a separator rather than ` = ` for ini setting

--- manifests/certificates/mountpoint.pp
+++ manifests/certificates/mountpoint.pp
@@ -43,12 +43,13 @@ class wazuh::certificates::mountpoint (
   }
 
   ini_setting { 'wazuh certificates mountpoint':
-    ensure       => present,
-    path         => $fileserver_conf,
-    section      => $bucket_name,
-    setting      => 'path',
-    value        => "${filebucket_path}/${bucket_name}",
-    indent_width => 4,
-    notify       => $_tonotify,
+    ensure            => present,
+    path              => $fileserver_conf,
+    section           => $bucket_name,
+    setting           => 'path',
+    value             => "${filebucket_path}/${bucket_name}",
+    indent_width      => 4,
+    key_val_separator => ' ',
+    notify            => $_tonotify,
   }
 }
