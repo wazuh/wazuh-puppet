84ccf7ce8493b59f7759762b9605067250f57b0c
vcerenu <victor.erenu@wazuh.com>
2025-09-05 13:49:33 -0300

    Add RHEL 8 and 9 support

--- manifests/params_manager.pp
+++ manifests/params_manager.pp
@@ -477,6 +477,32 @@ class wazuh::params_manager {
                   },
                 }
               }
+              if ( $facts['os']['release']['full'] =~ /^8.*/ ) {
+                $ossec_service_provider = 'systemd'
+                $api_service_provider = 'systemd'
+                $wodle_openscap_content = {
+                  'ssg-rhel-8-ds.xml' => {
+                    'type' => 'xccdf',
+                    profiles => ['xccdf_org.ssgproject.content_profile_pci-dss', 'xccdf_org.ssgproject.content_profile_common',],
+                  },
+                  'cve-redhat-8-ds.xml' => {
+                    'type' => 'xccdf',
+                  },
+                }
+              }
+              if ( $facts['os']['release']['full'] =~ /^9.*/ ) {
+                $ossec_service_provider = 'systemd'
+                $api_service_provider = 'systemd'
+                $wodle_openscap_content = {
+                  'ssg-rhel-9-ds.xml' => {
+                    'type' => 'xccdf',
+                    profiles => ['xccdf_org.ssgproject.content_profile_pci-dss', 'xccdf_org.ssgproject.content_profile_common',],
+                  },
+                  'cve-redhat-9-ds.xml' => {
+                    'type' => 'xccdf',
+                  },
+                }
+              }
             }
             'Fedora': {
               if ( $facts['os']['release']['full'] =~ /^(23|24|25).*/ ) {
