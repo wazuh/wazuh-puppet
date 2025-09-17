5dea942ccffc9d56af215ab934d8124c63ab4088
vcerenu <victor.erenu@wazuh.com>
2025-09-17 15:21:53 -0300

    Changes

--- manifests/params_manager.pp
+++ manifests/params_manager.pp
@@ -404,7 +404,7 @@ class wazuh::params_manager {
                 }
               }
             }
-            /^(wheezy|stretch|buster|bullseye|bookworm|trixie|sid|precise|trusty|vivid|wily|xenial|bionic|focal|groovy|jammy)$/: {
+            /^(wheezy|stretch|buster|bullseye|bookworm|trixie|sid|precise|trusty|vivid|wily|xenial|bionic|focal|groovy|jammy|noble)$/: {
               $server_service = 'wazuh-manager'
               $server_package = 'wazuh-manager'
               $wodle_openscap_content = undef
